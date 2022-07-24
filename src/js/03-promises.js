656db2d6dae9643525e47ec98f4ba7a198588578
BogdanGrynjuk <bgrynjuk@ukr.net>
2022-07-24 14:24:51 +0300

    updated task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,57 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+const inputDelay = document.querySelector('input[name="delay"]');
+const inputStep = document.querySelector('input[name="step"]');
+const inputAmount = document.querySelector('input[name="amount"]');
+const submit = document.querySelector('button[type="submit"]');
+
+submit.addEventListener('click', onSubmit);
+
+function onSubmit(event) {
+  event.preventDefault();
+  console.clear();
+
+  if (!inputDelay.value || !inputStep.value || !inputAmount.value) {
+    alert('Будь ласка заповніть всі поля форми');
+    return;
   }
+  
+  let delay = Number(inputDelay.value);
+
+  for (let position = 1; position <= Number(inputAmount.value); position += 1) {
+    
+    createPromise(position, delay)
+      .then((resolve) => {
+        console.log(resolve);
+      })
+      .catch((reject) => {
+        console.log(reject);
+      });
+    
+    delay += Number(inputStep.value);    
+  }  
+
+  clearFormFields();
 }
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+
+    setTimeout(() => {
+      if (shouldResolve) {
+        // Fulfill
+        resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      } else {
+        // Reject
+        reject(`❌ Rejected promise ${position} in ${delay}ms`);
+      }
+    }, delay);    
+  });  
+}
+
+function clearFormFields() {
+  inputDelay.value = '';
+  inputStep.value = '';
+  inputAmount.value = '';
+}
+
+
