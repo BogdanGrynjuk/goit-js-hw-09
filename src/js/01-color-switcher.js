f28029d776f5bec81a5569aeb7eecc2befc121a0
BogdanGrynjuk <bgrynjuk@ukr.net>
2022-07-24 10:27:55 +0300

    updated task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,2 +1,25 @@
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
+let inervalID = null;
 
-console.log('hello')
+btnStart.addEventListener('click', onClickStart);
+btnStop.addEventListener('click', onClickStop);
+
+function onClickStart() {
+  inervalID = setInterval(setRandomBackground, 1000);
+  btnStart.disabled = true;
+} 
+
+function onClickStop() {
+  clearInterval(inervalID);
+  btnStart.disabled = false;
+}
+
+function setRandomBackground() {
+  body.style.backgroundColor = getRandomHexColor();
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
